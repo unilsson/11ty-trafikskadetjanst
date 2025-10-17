478445ec595a67af469382b8203b839a511a2ac6
unilsson <unilsson@ipfabriken.se>
2025-10-17 11:44:04 +0200

    Fixing css

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,10 @@ export default function(eleventyConfig) {
   // Copy images straight to the output folder
   eleventyConfig.addPassthroughCopy("src/images");
 
+  // Copy css to the output folder
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addWatchTarget("src/css");
+
   // (optional) live-reload when images change
   eleventyConfig.addWatchTarget("src/images");
 
