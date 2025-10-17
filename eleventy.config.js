3a96a22bfeb65ed9c13d659a5d18a27a698b2202
unilsson <unilsson@ipfabriken.se>
2025-10-17 14:21:11 +0200

    Adding image-circulator script on homepage

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,9 +2,16 @@ export default function(eleventyConfig) {
   // Copy images straight to the output folder
   eleventyConfig.addPassthroughCopy("src/images");
 
+    // Copy circulator-images straight to the output folder
+  eleventyConfig.addPassthroughCopy("src/circulator-images");
+
   // Copy css to the output folder
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addWatchTarget("src/css");
+
+    // Copy css to the output folder
+  eleventyConfig.addPassthroughCopy("src/javascript");
+  eleventyConfig.addWatchTarget("src/javascript");
 
   // (optional) live-reload when images change
   eleventyConfig.addWatchTarget("src/images");
